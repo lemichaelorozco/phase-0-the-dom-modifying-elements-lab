3ff802e6b0f1c3be6c0f2671b65f4d4d7cd10718
Michael Orozco <colomike925@gmail.com>
2022-09-29 23:24:56 -0400

    commit tree c4960db449aae27abd8bc63e6986d45bbef12f12

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+let element1 = document.getElementById("main");
+
+let newHeader = document.createElement("h1");
+
+
+
+
+
+
+element1.remove();
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Michael is the champion";
